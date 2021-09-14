d4b003c9cc9c1add2df6b719a0202e5c733603a6
RkTm100 <70105389+RkTm100@users.noreply.github.com>
2021-09-13 21:53:58 -0500

    Add files via upload

---
+++ bellcurveproject.py
@@ -0,0 +1,8 @@
+import plotly.figure_factory as ff
+import pandas as pd
+import csv
+
+df = pd.read_csv("https://raw.githubusercontent.com/whitehatjr/bell-curve--normal-distribution/master/data.csv")
+print(df.head())
+fig = ff.create_distplot([df["Avg Rating"].tolist()], ["Avg Rating"])
+fig.show()
